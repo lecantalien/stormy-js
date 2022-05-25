c3505ee6e5f099137f8ee13bc778b48bfab445d3
lecantalien <clipperdu15@hotmail.fr>
2022-05-25 16:55:05 +0200

    add bootstrap add user-title add display-warning add display-help-cli add cli

--- src/main.js
+++ src/main.js
@@ -1,4 +1,29 @@
 import { createApp } from 'vue'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+import "bootstrap/js/dist/base-component"
+import "bootstrap/js/dist/popover"
+import "bootstrap/js/dist/dropdown"
+import "bootstrap/js/dist/collapse"
+/**
+ * import LIB toast
+ *@see https://github.com/Maronato/vue-toastification/tree/next
+ *  **/
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
+
+import App from './App.vue'
+import router from './router'
+
+// Import custom css
+import "../sass/main.scss";
+
+const vm = createApp(App);
+
+//option for toastification
+const options = {
+    // You can set your default options here
+};
+
+    vm.use(Toast,options)
+    vm.use(router)
+    vm.mount('#app')
---
+++ src/mixin/ToastService.js
@@ -0,0 +1,13 @@
+import { useToast } from "vue-toastification";
+
+export const ToastService = {
+    props: {},
+    data() {
+        return {
+            AppToast: useToast(),
+        }
+    },
+
+    computed: {},
+    methods: {},
+}
